5660c4312115e15f74dffcedcef6b3c88cb50f70
Igor Novikov <volga322@yandex.ru>
2020-05-15 23:52:43 +0300

    Dynamic articles

--- FirstDjango/apps/articles/urls.py
+++ FirstDjango/apps/articles/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'articles'
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('test/', views.test, name = 'test')
+    path('<int:article_id>/', views.detail, name = 'detail'),
+    path('<int:article_id>/put_comment/', views.put_comment, name = 'put_comment')
 ]
--- FirstDjango/apps/articles/views.py
+++ FirstDjango/apps/articles/views.py
@@ -1,7 +1,24 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from .models import Article
 
 def index(request):
-    return HttpResponse("Hello World")
+    last_articles_list = Article.objects.order_by('-pub_date')[:5]
+    return render(request, 'articles/list.html', {'last_articles_list': last_articles_list})
 
-def test(request):
-    return HttpResponse("Test Me")
+def detail(request, article_id):
+    try:
+        a = Article.objects.get( id = article_id )
+    except:
+        raise Http404("Article is not found")
+    last_comments_list = a.comment_set.order_by('-id')[:10]
+    return render(request, 'articles/detail.html', {'article': a, 'last_comments_list': last_comments_list})
+
+def put_comment(request, article_id):
+    try:
+        a = Article.objects.get( id = article_id )
+    except:
+        raise Http404("Article is not found")
+    a.comment_set.create(author_name = request.POST['name'], comment_text = request.POST['text'])
+    return HttpResponseRedirect(reverse('articles:detail', args=(a.id,)))
